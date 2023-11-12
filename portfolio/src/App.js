1a26ae48252bfa1d90d29232aa10b3dae145abf6
Nurkulova <cholponnurkulova@gmail.com>
2023-11-12 22:50:57 +0600

    created components and added general styles

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/nav/Navbar";
+import About from "./components/about/About";
+import Experiense from "./components/experiense/Experiense";
+import Services from "./components/services/Services";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contacts from "./components/contacts/Contacts";
+import Portfolio from "./components/portfolio/Portfolio";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+        <Header />
+        <Navbar />
+        <About />
+        <Experiense/>
+        <Services />
+        <Portfolio/>
+        <Testimonials/>
+        <Contacts />
+        <Footer />
       </div>
     );
   }
---
+++ portfolio/src/components/experiense/Experiense.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './experiense.css'
+
+const Experiense = () => {
+  return (
+    <div>Experiens</div>
+  )
+}
+
+export default Experiense
